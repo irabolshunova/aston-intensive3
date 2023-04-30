e878313fe12e3e08b1551345bc0fb287ef3acbed
irabolshunova <ira.bolshunova@gmail.com>
2023-04-30 14:52:29 +0400

    add task

---
+++ src/main/java/ru/irabolshunova/connection/ConnectionUtil.java
@@ -0,0 +1,17 @@
+package ru.irabolshunova.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class ConnectionUtil {
+    private ConnectionUtil() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "postgres";
+        return DriverManager.getConnection(url, username, password);
+    }
+}
+
---
+++ src/main/java/ru/irabolshunova/model/Book.java
@@ -0,0 +1,42 @@
+package ru.irabolshunova.model;
+
+public class Book {
+    public class Book {
+        private int id;
+        private String title;
+        private String author;
+        private User user;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+    }
+}
